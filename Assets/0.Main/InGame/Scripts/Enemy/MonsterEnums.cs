4f33540475523c4497b3968f55c02103ca66e407
briankunkel9 <briankunkel9@gmail.com>
2025-07-07 21:33:32 +0900

    [몬스터 타입 구분을 위한 enum 추가] EnemyAttack 공격유형 구분 근거리,원거리 EnemyMovement 이동유형 직신, 웨이브, 등등

---
+++ Assets/0.Main/InGame/Scripts/Enemy/MonsterEnums.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EnemyAttackType
+{
+    Melee,
+    Range,
+}
+
+public enum EnemyMovementType
+{
+    Direct,
+    Wave,
+}
