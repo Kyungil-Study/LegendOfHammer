fbff39e37224d593a44e310f71368f0cc799725a
박승식 <agagtmdtlr>
2025-07-07 14:31:02 +0900

    전투 이벤트 콜백 함수 추가

--- Assets/0.Main/InGame/Scripts/BattleEventManager.cs
+++ Assets/0.Main/InGame/Scripts/BattleEventManager.cs
@@ -7,15 +7,27 @@ using UnityEngine;
 
 public class BattleEventManager : MonoSingleton<BattleEventManager>
 {
-    // Start is called before the first frame update
-    void Start()
+    public class EventCallbacks
     {
-        
+        public Action<StartBattleEventArgs> OnStartBattle;
+        public Action<EndBattleEventArgs> OnEndBattle;
+        public Action<TakeDamageEventArgs> OnTakeDamage;
     }
+    public EventCallbacks Callbacks { get; private set; } = new EventCallbacks();
 
-    // Update is called once per frame
-    void Update()
+    public void CallEvent(BattleEventArgs eventArgs)
     {
-        
+        if(eventArgs is StartBattleEventArgs startEvent)
+        {
+            Callbacks.OnStartBattle?.Invoke(startEvent);
+        }
+        else if(eventArgs is EndBattleEventArgs endEvent)
+        {
+            Callbacks.OnEndBattle?.Invoke(endEvent);
+        }
+        else if(eventArgs is TakeDamageEventArgs damageEvent)
+        {
+            Callbacks.OnTakeDamage?.Invoke(damageEvent);
+        }
     }
 }
