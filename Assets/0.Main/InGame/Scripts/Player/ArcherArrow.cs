af9ae197796782d966ed534e467e72bfff805d8c
gyusummer <gyusummer@gmail.com>
2025-07-07 19:05:30 +0900

    [궁수]
    화살 생성
    공격 활 애니메이션

--- Assets/0.Main/InGame/Scripts/Player/ArcherArrow.cs
+++ Assets/0.Main/InGame/Scripts/Player/ArcherArrow.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Searcher;
 using UnityEngine;
 
 public class ArcherArrow : MonoBehaviour
 {
     private bool mb_IsFired = false;
+    public int damage = 10;
     private Vector3 m_TargetDirection;
     private float m_Speed = 1f;
     private float m_LifeTime = 5f;
@@ -31,11 +33,28 @@ public class ArcherArrow : MonoBehaviour
             m_TargetDirection = target.transform.position - transform.position;
             m_TargetDirection.Normalize();
         }
+        else
+        {
+            m_TargetDirection = Vector3.up;
+        }
+
+        mb_IsFired = true;
     }
 
     private GameObject FindTarget()
     {
         // TODO: Implement target finding logic and return the target Monster.
         return null;
+    }
+
+    private void OnHit()
+    {
+        TakeDamageEventArgs eventArgs = new TakeDamageEventArgs(
+            Squad.Instance,
+            FindTarget().GetComponent<IBattleCharacter>(), 
+            damage
+        );
+        BattleEventManager.Instance.CallEvent(eventArgs);
+        Destroy(gameObject);
     }
 }
