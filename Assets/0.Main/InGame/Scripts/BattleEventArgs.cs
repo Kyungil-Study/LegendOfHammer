660ac717ecbeb2c03c214e782ca337a051cb7b01
박승식 <agagtmdtlr>
2025-07-07 14:01:03 +0900

    [프로젝트 개발 환경 구축] monosingleton tsvloader 현재 프로젝트 테으블 규칙에 맞춰서 config 변경 event arg 추가

--- Assets/0.Main/InGame/Scripts/BattleEventArgs.cs
+++ Assets/0.Main/InGame/Scripts/BattleEventArgs.cs
@@ -2,17 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BattleEventArgs : MonoBehaviour
+public class BattleEventArgs : System.EventArgs
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+}
 
-    // Update is called once per frame
-    void Update()
+
+public class StartBattleEventArgs : BattleEventArgs
+{
+    public Player player;
+    public Monster monster;
+
+    public StartBattleEventArgs(Player player, Monster monster)
     {
-        
+        this.player = player;
+        this.monster = monster;
     }
 }
+
+public class EndBattleEventArgs : BattleEventArgs
+{
+    public Player player;
+    public Monster monster;
+    public bool isVictory;
+
+    public EndBattleEventArgs(Player player, Monster monster, bool isVictory)
+    {
+        this.player = player;
+        this.monster = monster;
+        this.isVictory = isVictory;
+    }
+}
+
--- Assets/0.Main/InGame/Scripts/BattleManager.cs
+++ Assets/0.Main/InGame/Scripts/BattleManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BattleManager : MonoBehaviour
+public class BattleManager : MonoSingleton<BattleManager>
 {
     // Start is called before the first frame update
     void Start()
