18c97779a2f27cd9a824a2ebd43e1292169ac816
gyusummer <gyusummer@gmail.com>
2025-07-07 17:14:44 +0900

    [용사단 기초 일부]
    간단한 조작 추가
    궁수 화살 클래스 추가

---
+++ Assets/0.Main/InGame/Scripts/Player/Archer.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Archer : Hero
+{
+    public Transform arrowSpawnPoint;
+    public ArcherArrow arrowPrefab;
+    private void Awake()
+    {
+        attackDamage = 250;
+        attackPerSec = 2;
+    }
+
+    private void Update()
+    {
+        attackCooldown -= Time.deltaTime;
+        if (attackCooldown <= 0)
+        {
+            Attack();
+            ApplyCooldown();
+        }
+    }
+
+    public override void Attack()
+    {
+        Instantiate(arrowPrefab, arrowSpawnPoint.position, arrowSpawnPoint.rotation);
+    }
+}
---
+++ Assets/0.Main/InGame/Scripts/Player/SquadController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquadController : MonoBehaviour
+{
+    public Squad squad;
+    private Vector2 m_TouchStartPosition;
+    
+    private void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            if (Input.touches[0].phase == TouchPhase.Began)
+            {
+                m_TouchStartPosition = Input.touches[0].position;
+            }
+            else
+            {
+                Vector3 dir = Input.touches[0].position - m_TouchStartPosition;
+                dir.Normalize();
+
+                squad.transform.position += dir * (squad.stats.moveSpeed * Time.deltaTime);
+            }
+        }
+    }
+}
