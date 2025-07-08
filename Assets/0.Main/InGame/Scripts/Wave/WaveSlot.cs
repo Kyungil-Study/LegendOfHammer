98f6c84e8a2698c9d7674c2b4622e2efa06b5e11
박승식 <agagtmdtlr>
2025-07-08 17:52:03 +0900

    [노멀 웨이브 프리팹 추가] three,four, five, six

---
+++ Assets/0.Main/InGame/Scripts/Wave/WaveSlot.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSlot : MonoBehaviour
+{
+    [SerializeField] private EnemyAttackType attackType;
+}
