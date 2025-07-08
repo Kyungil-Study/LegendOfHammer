bb32694d59922c5e036c168c8e152c2e1b0f7a44
박승식 <agagtmdtlr>
2025-07-08 13:56:11 +0900

    [audience 프리팹 추가] 맵 주변에 구경하는 구경꾼

--- Assets/0.Main/InGame/Scripts/Stage/Audience.cs
+++ Assets/0.Main/InGame/Scripts/Stage/Audience.cs
@@ -2,19 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer), typeof(Animator))]
 public class Audience : MonoBehaviour
 {
+    [SerializeField] private AnimationClip[] audienceAnimation;
+    [SerializeField] private float ScaleMin = 3f;
+    [SerializeField] private float ScaleMax = 5f;
+    [SerializeField] private int SortingOrderMin = 1000; // 초기화 시간
+    [SerializeField] private int SortingOrderMax = 2000; // 초기화 시간
     
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float normalizedInitTime = 0f;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
 
-    // Update is called once per frame
-    void Update()
+    public void Setup()
     {
+        transform.localScale = Vector3.one * Random.Range(ScaleMin, ScaleMax);
         
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sortingOrder = Random.Range(SortingOrderMin, SortingOrderMax);
+        
+        animator = GetComponent<Animator>();
+        
+        var animatorOverrideController = new AnimatorOverrideController(animator.runtimeAnimatorController);
+        animatorOverrideController.name = "AudienceOverrideController";
+        var clipIndex = Random.Range(0, audienceAnimation.Length);
+        Debug.Log($"clip Index : " + clipIndex);
+        
+        animatorOverrideController["Audience_Idle"] = audienceAnimation[clipIndex];
+        animator.runtimeAnimatorController = animatorOverrideController;
+        animator.Play("Audience_Idle", 0, Random.Range(0f, 1f));
     }
 }
