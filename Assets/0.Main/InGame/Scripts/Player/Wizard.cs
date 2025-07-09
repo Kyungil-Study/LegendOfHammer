321c723c731591c826add03358a8e21c92cc0869
gyusummer <gyusummer@gmail.com>
2025-07-09 15:58:02 +0900

    [마법사 공격 폭발 애니메이션 추가]

--- Assets/0.Main/InGame/Scripts/Player/Wizard.cs
+++ Assets/0.Main/InGame/Scripts/Player/Wizard.cs
@@ -18,7 +18,6 @@ public class Wizard : Hero
     // TODO: 중복 공격 시 피해 감소율
     // 마법사 마법 구체 폭발 피해량
     // [[{(마법사 기본 공격 피해량 x 치명타 피해량) + 타격 당 데미지} x 받는 피해 증가] x 최종 데미지 증가] x 중복 공격 시 피해 감소율
-
     protected override int CalculateDamage(bool isCritical = false)
     {
         float critFactor = isCritical ? baseStats.CriticalDamage : 1f;
