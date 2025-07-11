1e924391f2e979d139c1b156c1bc7ca60d488fb2
gyusummer <gyusummer@gmail.com>
2025-07-11 17:59:24 +0900

    [쌤 클램프 어쩌구 검토중]

---
+++ Assets/1.Develop/Gyuha/JobTang.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JobTang : MonoBehaviour
+{
+    public ClampedInt hp;
+
+    private void Start()
+    {
+        uint a = 10;
+        uint b = 20;
+    }
+}
