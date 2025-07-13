e02cd98214cbca701900a93dccd051b23b11c3e7
박승식 <briankunkel9@gmail.com>
2025-07-13 12:59:57 +0900

    [로딩씬 인게임씬 합침]
    - 테스트 하기 불편해서 로딩씬은 제거하고, 인게임에서 UI를 오버레이 해서 로딩중이라고 알 수 있도록 처리하고,로딩 처리 인게임으로 옮겨놈

--- Assets/0.Main/Scripts/SessionManager.cs
+++ Assets/0.Main/Scripts/SessionManager.cs
@@ -17,60 +17,18 @@ public class SessionManager : SingletonBase<SessionManager>
 
     private void SceneLoaded(Scene scene, LoadSceneMode arg1)
     {
-        if(scene.name == "Scene_Loading")
-        {
-            Debug.Log("Loading scene loaded.");
-            LoadAllResourcesAsync();
-        }
-        else if (scene.name == "Scene_Dungeon")
+        if (scene.name == "Scene_Dungeon")
         {
             Debug.Log("Dungeon scene loaded.");
-            StartGame();
         }
     }
 
-    public void GoToLoadScene()
-    {
-        Debug.Log($"[SessionManager] Going to load scene");
-        
-        // Load the loading scene
-        SceneManager.LoadScene("Scene_Loading", LoadSceneMode.Single);
-    }
-
-    public async Task LoadAllResourcesAsync()
-    {
-        var loadables = FindObjectsOfType<MonoBehaviour>().OfType<ILoadable>().Where(l => !l.IsLoaded).ToList();
-        var tasks = loadables.Select(l => l.LoadAsync()).ToArray();
-
-        LoadCompleteEventArg[] results = await Task.WhenAll(tasks);
-
-        // 결과 처리 (성공/실패 여부)
-        if (results.All(r => r.Success))
-        {
-            Debug.Log("모든 리소스 로드 완료");
-            GoToGameScene();
-        }
-        else
-        {
-            foreach(var r in results.Where(r => !r.Success))
-                Debug.LogError($"로드 실패: {r.ErrorMessage}");
-            // 실패시 처리
-        }
-        
-    }
-
-    void StartGame()
-    {
-        Debug.Log($"[SessionManager] Starting game");
-        var stageIndex = BackendStageGameData.stage.Currentstage;; // For testing purposes, remove later
-        BattleManager.Instance.StartGame(stageIndex);
-    }
 
     public void GoToGameScene() 
     { // todo: 로딩 연동 필요
 
         Debug.Log($"[SessionManager] Starting game");
-        var stageIndex = BackendStageGameData.stage.Currentstage;; // For testing purposes, remove later
+        
         SceneManager.LoadScene("Scene_Dungeon", LoadSceneMode.Single);
 
     }
@@ -88,6 +46,6 @@ public class SessionManager : SingletonBase<SessionManager>
         }
 
         Debug.Log($"[SessionManager] Ending game");
-        SceneManager.LoadScene("Scene_Loading", LoadSceneMode.Single);
+        SceneManager.LoadScene("Scene_Lobby", LoadSceneMode.Single);
     }
 }
