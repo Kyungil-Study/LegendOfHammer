5866caaa2a4cc1b97fe6c31e0b2e53c7e6c21283
문현승 <mhs990209@gmail.com>
2025-07-14 16:25:57 +0900

    [적 HP + 아군 HP 추가]
    적 HP는 작동 확인
    아군 HP는 확인 필요

---
+++ Assets/0.Main/Scripts/HP/MonsterHPUIManager.cs
@@ -0,0 +1,67 @@
+using System.Collections.Generic;
+using UnityEngine;
+using System.Reflection;
+
+public class MonsterHPUIManager : MonoBehaviour
+{
+    [SerializeField] private RectTransform hpBarParent;  // UICanvas 내 HP바 부모
+    [SerializeField] private MonsterHPBar hpBarPrefab;   // 프리팹
+
+    private readonly Dictionary<MonoBehaviour, MonsterHPBar> activeBars = new();
+
+    public void RegisterMonster(MonoBehaviour monster)
+    {
+        if (activeBars.ContainsKey(monster)) return;
+
+        var bar = Instantiate(hpBarPrefab, hpBarParent);
+        activeBars[monster] = bar;
+        bar.AttachTo(monster.transform);
+    }
+
+    public void UnregisterMonster(MonoBehaviour monster)
+    {
+        if (activeBars.TryGetValue(monster, out var bar))
+        {
+            Destroy(bar.gameObject);
+            activeBars.Remove(monster);
+        }
+    }
+
+    void LateUpdate()
+    {
+        foreach (var pair in activeBars)
+        {
+            var monster = pair.Key;
+            var bar = pair.Value;
+
+            if (monster == null)
+            {
+                UnregisterMonster(pair.Key);
+                continue;
+            }
+
+            bar.UpdatePosition();
+
+            // 🔥 체력 업데이트
+            float ratio = GetHealthRatio(monster);
+            bar.SetRatio(ratio);
+        }
+    }
+
+    private float GetHealthRatio(MonoBehaviour monster)
+    {
+        var type = monster.GetType();
+
+        FieldInfo currentHpField = type.GetField("mCurrentHP", BindingFlags.NonPublic | BindingFlags.Instance);
+        FieldInfo maxHpField     = type.GetField("mMaxHP", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        if (currentHpField == null || maxHpField == null) return 1f;
+
+        int current = (int)currentHpField.GetValue(monster);
+        int max     = (int)maxHpField.GetValue(monster);
+
+        if (max <= 0) return 1f;
+        return Mathf.Clamp01((float)current / max);
+    }
+
+}
