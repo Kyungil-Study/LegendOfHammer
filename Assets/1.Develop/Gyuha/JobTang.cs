c3556e807e9c9f5c52c54f5263faf48e927b04c6
gyusummer <gyusummer@gmail.com>
2025-07-15 12:02:54 +0900

    [공격 우선순위 설정, 용사단 애니메이션]
    배틀 매니저에 몹 리스트 추가

--- Assets/1.Develop/Gyuha/JobTang.cs
+++ Assets/1.Develop/Gyuha/JobTang.cs
@@ -1,13 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class JobTang : MonoBehaviour
 {
-    public ClampedInt hp;
+    public List<Hero> heroes = new List<Hero>();
 
     private void Start()
     {
-        
+        var result = heroes.Select(hero =>
+        {
+            if (hero.baseAttackDamage > 300)
+            {
+                return hero;
+            }
+
+            return null;
+        });
+
+        foreach (var hero in result)
+        {
+            if (hero != null)
+            {
+                Debug.Log(hero.name);
+            }
+            else
+            {
+                Debug.Log("null");
+            }
+        }
     }
 }
