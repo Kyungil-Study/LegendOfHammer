efa7563b798f4aa3afaa6cb4dff4adddc537e579
gyusummer <gyusummer@gmail.com>
2025-07-15 14:14:50 +0900

    [궁수 공격 우선순위 수정]
    거리 기준 추가

--- Assets/0.Main/InGame/Scripts/Player/ArcherArrow.cs
+++ Assets/0.Main/InGame/Scripts/Player/ArcherArrow.cs
@@ -10,6 +10,14 @@ public class ArcherArrow : HeroProjectile
     public int penetrateLimit = 0;
     public Monster targetMonster;
     
+    /// <summary>
+    /// 등급, 최대 체력, 거리를 기준으로 정렬 후 가장 먼저 나오는 몬스터를 타겟으로 설정합니다.
+    /// <br />* 등급이 높은 몬스터가 우선
+    /// <br />* 등급이 같다면 최대 체력이 높은 몬스터가 우선
+    /// <br />* 최대 체력도 같다면 플레이어와의 거리가 가까운 몬스터가 우선
+    /// <br />// 만약 타겟이 없다면 null을 반환합니다.
+    /// </summary>
+    /// <returns></returns>
     protected override GameObject FindTarget()
     {
        var monsters =  BattleManager.Instance.GetAllMonsters();
@@ -18,12 +26,14 @@ public class ArcherArrow : HeroProjectile
            Debug.Log("No monsters found to target.");
            return null;
        }
+
        monsters = monsters.OrderByDescending(monster =>
-       {
-           var id = monster.EnemyID;
-           var data = EnemyDataManager.Instance.Records[id];
-           return data.Enemy_Rank;
-       }).ThenByDescending(monster => monster.MaxHP);
+           {
+               var id = monster.EnemyID;
+               var data = EnemyDataManager.Instance.Records[id];
+               return data.Enemy_Rank;
+           }).ThenByDescending(monster => monster.MaxHP)
+           .ThenBy(monster => Vector3.Distance(monster.transform.position, transform.position));
        return monsters.First().gameObject;
     }
 
