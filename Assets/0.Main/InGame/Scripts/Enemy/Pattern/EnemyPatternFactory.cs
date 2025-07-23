4bc8ad81b3b1a12fc7aebb04a801a7d4697bf2b1
GW <publicman22@naver.com>
2025-07-23 16:16:25 +0900

    [리팩토링 거의 완료]
    이동 + 공격 로직 전략 + 팩토리 패턴으로 수정 완료

--- Assets/0.Main/InGame/Scripts/Enemy/Pattern/EnemyPatternFactory.cs
+++ Assets/0.Main/InGame/Scripts/Enemy/Pattern/EnemyPatternFactory.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public static class MovementFactory
 {
+    
     public static IMoveBehaviour Create(EnemyMovementPattern type, Monster monster)
     {
         return type switch
         {
             EnemyMovementPattern.Straight => new StraightMove(),
+            EnemyMovementPattern.Zigzag   => new ZigzagMove(monster.ZigzagConfig),
+            EnemyMovementPattern.Chase    => new ChaseMove(monster.ChaseConfig),
+            EnemyMovementPattern.Flying   => new FlyingMove(monster.FlyingMoveConfig),
             _ => null
         };
     }
@@ -21,6 +25,12 @@ public static class AttackFactory
         return type switch
         {
             EnemyAttackPattern.Normal  => new NormalAttack(),
+            EnemyAttackPattern.Suicide => new SuicideAttack(monster.SuicideCfg),
+            EnemyAttackPattern.Shield  => new ShieldAttack(monster.ShieldCfg),
+            EnemyAttackPattern.Sniper  => new SniperAttack(monster.SniperCfg),
+            EnemyAttackPattern.Spread  => new SpreadAttack(monster.SpreadCfg),
+            EnemyAttackPattern.Radial  => new RadialAttack(monster.RadialCfg),
+            EnemyAttackPattern.Flying  => new FlyingAttack(monster.FlyingAtkCfg),
             _ => null
         };
     }
