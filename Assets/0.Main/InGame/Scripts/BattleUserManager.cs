91ce1fceb4e7dab64adfb9fff122387001ca0bbb
박승식 <agagtmdtlr>
2025-07-24 15:23:28 +0900

    [Easy Save 에셋 추가]

--- Assets/0.Main/InGame/Scripts/BattleUserManager.cs
+++ Assets/0.Main/InGame/Scripts/BattleUserManager.cs
@@ -14,9 +14,14 @@ public class UserAugmentData
     public string CommonAugment { get; set; }
 }
 
-public class BattleUserManager : MonoSingleton<BattleUserManager>
+public class BattleUserManager : SingletonBase<BattleUserManager>
 {
     public UserStageData StageData { get; private set; } = new UserStageData();
     public UserAugmentData AugmentData { get; private set; } = new UserAugmentData();
 
+    public override void OnInitialize()
+    {
+        base.OnInitialize();
+        // 초기화 로직을 여기에 추가할 수 있습니다.
+    }
 }
