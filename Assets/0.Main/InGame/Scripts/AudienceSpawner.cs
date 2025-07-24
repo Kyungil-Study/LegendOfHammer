b773b0c06a179a3087f3f97f5673acbd388371f0
박승식 <briankunkel9@gmail.com>
2025-07-24 22:21:56 +0900

    [추격 군중 추가]

--- Assets/0.Main/InGame/Scripts/AudienceSpawner.cs
+++ Assets/0.Main/InGame/Scripts/AudienceSpawner.cs
@@ -25,12 +25,27 @@ public class AudienceSpawner : MonoBehaviour
     private Grid grid;
     private Tilemap tilemap;
     // Start is called before the first frame update
+    
+    [SerializeField] private AnimatorOverrideController audienceAOC;
+    [SerializeField] private AnimationClip[] audienceAnimations;
+    Dictionary<AnimationClip,AnimatorOverrideController> audienceAOCMap = new Dictionary<AnimationClip, AnimatorOverrideController>();
+    public Dictionary<AnimationClip, AnimatorOverrideController> AudienceAOCMap => audienceAOCMap;
+    public AnimationClip[] AudienceAnimations => audienceAnimations;
+    
     void Start()
     {
         tilemapRenderer = GetComponent<TilemapRenderer>();
         tilemapRenderer.enabled = false;
         tilemap = GetComponent<Tilemap>();
         grid = GetComponentInParent<Grid>();
+
+        foreach (AnimationClip audienceClip in audienceAnimations)
+        {
+            var animatorOverrideController = new AnimatorOverrideController(audienceAOC);
+            animatorOverrideController.name = $"AOC_{audienceClip.name}";
+            animatorOverrideController["Audience_Idle"] = audienceClip;
+            AudienceAOCMap[audienceClip] = animatorOverrideController;
+        }
         
         float offset = 1f / spawnCountPerTile; // 타일의 절반 크기만큼 오프셋 설정
         for(int i = 0 ;i < spawnCountPerTile; i++)
@@ -42,7 +57,7 @@ public class AudienceSpawner : MonoBehaviour
             audience.transform.position = spawnPosition;
             audience.transform.rotation = SpawRotation;
                     
-            audience.Setup();
+            audience.Setup(this);
         }
     }
 }
