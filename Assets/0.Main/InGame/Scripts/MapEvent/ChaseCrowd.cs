5d105e50a4aadc0c990f99bd31cc534438e8c0f0
박승식 <briankunkel9@gmail.com>
2025-07-27 19:00:49 +0900

    [메테오 이벤트 추가]
    플레이어 주변에 랜덤으로 공격
    땅에 도달하여 폭발하는 동안에 플레이어에게 피해를 입힘
    추격군과 맵이벤트 매니저로 통합

--- Assets/0.Main/InGame/Scripts/MapEvent/ChaseCrowd.cs
+++ Assets/0.Main/InGame/Scripts/MapEvent/ChaseCrowd.cs
@@ -4,9 +4,10 @@ using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Serialization;
 
 [RequireComponent(typeof(BoxCollider2D))]
-public class ChaseCrowd : MonoBehaviour , IBattleCharacter
+public class ChaseCrowd : MapEventExecutor , IBattleCharacter
 {
     Vector3 originPosition;
     [SerializeField] private Transform destination; // 추적할 대상 위치
@@ -17,7 +18,7 @@ public class ChaseCrowd : MonoBehaviour , IBattleCharacter
     [SerializeField] bool isChasing = false;
     [SerializeField] private Ease chaseEase = Ease.Linear; // 추적 속도 계수
     
-    [SerializeField] private AttackSignal attackSignal; // 추적 공격 시그널
+    [FormerlySerializedAs("attackSignal")] [SerializeField] private AttackAlertSignal attackAlertSignal; // 추적 공격 시그널
     [SerializeField] private BoxCollider2D attackCollider; // 추적 공격 콜라이더
     
     [SerializeField] private float attackPower = 10f; // 추적 공격력
@@ -44,34 +45,20 @@ public class ChaseCrowd : MonoBehaviour , IBattleCharacter
         var ratio = arg1 / arg2;
         isChasing = ratio >= normalizedTriggerGauge;
     }
-
-    private IEnumerator GoTo(Vector3 targetPosition)
-    {
-        yield return new WaitForSeconds(chaseSpeed);
-    }
-
     private IEnumerator ChaseCoroutine()
     {
-        while (gameObject.activeInHierarchy)
-        {
-            if (isChasing)
-            {
-                attackSignal.gameObject.SetActive(true);
-                yield return new WaitForSeconds(attackDelay);
-                attackCollider.enabled = true;
-                var gotoDestination = transform.DOMove(destination.position, chaseSpeed)
-                    .SetEase(chaseEase);
-                    //.OnComplete(() => Debug.Log($"Chased to {destination.name} at position {destination.position}"));
-                yield return gotoDestination.WaitForCompletion();
-                attackSignal.gameObject.SetActive(false);
-                var gotoOrigin = transform.DOMove(originPosition, chaseSpeed).SetEase(Ease.Linear);
-                yield return gotoOrigin.WaitForCompletion();
-                attackCollider.enabled = false;
-                yield return new WaitForSeconds(chaseAttackInterval);
-            }
-
-            yield return null;
-        }
+        transform.position = originPosition;
+        attackAlertSignal.gameObject.SetActive(true);
+        yield return new WaitForSeconds(attackDelay);
+        attackCollider.enabled = true;
+        var gotoDestination = transform.DOMove(destination.position, chaseSpeed)
+            .SetEase(chaseEase);
+        //.OnComplete(() => Debug.Log($"Chased to {destination.name} at position {destination.position}"));
+        yield return gotoDestination.WaitForCompletion();
+        attackAlertSignal.gameObject.SetActive(false);
+        var gotoOrigin = transform.DOMove(originPosition, chaseSpeed).SetEase(Ease.Linear);
+        yield return gotoOrigin.WaitForCompletion();
+        attackCollider.enabled = false;
     }
 
 
@@ -92,5 +79,11 @@ public class ChaseCrowd : MonoBehaviour , IBattleCharacter
     public void TakeDamage(TakeDamageEventArgs eventArgs)
     {
         // 추적 크라우드가 피해를 받았을 때 처리
+    }
+
+    public override void ExecuteMapEvent()
+    {
+        StopCoroutine(ChaseCoroutine());
+        StartCoroutine(ChaseCoroutine());
     }
 }
