a4fb2211b6791cad837b1addfa997525c385b5f5
박승식 <briankunkel9@gmail.com>
2025-07-27 19:00:49 +0900

    [메테오 이벤트 추가]
    플레이어 주변에 랜덤으로 공격
    땅에 도달하여 폭발하는 동안에 플레이어에게 피해를 입힘
    추격군과 맵이벤트 매니저로 통합

--- Assets/0.Main/InGame/Scripts/MapEvent/AttackAlertSignal.cs
+++ Assets/0.Main/InGame/Scripts/MapEvent/AttackAlertSignal.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AttackSignal : MonoBehaviour
+public class AttackAlertSignal : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
---
+++ Assets/0.Main/InGame/Scripts/MapEvent/Meteor/Meteor.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEditor.Search.Providers;
+using UnityEngine;
+
+[RequireComponent(typeof(PolygonCollider2D))]
+public class Meteor : MonoBehaviour , IBattleCharacter
+{
+    [SerializeField] private int damage = 10;
+    [SerializeField] private float fallDuration = 2f;
+    
+    [SerializeField] private GameObject explosion;
+    [SerializeField] private GameObject fall;
+    
+    [SerializeField] private Transform startPoint;
+    [SerializeField] private Transform endPoint;
+    
+    PolygonCollider2D attackCollider;
+    bool isHitted = false;
+    Animator explosionAnimator;
+
+    void Start()
+    {
+        explosionAnimator = explosion.GetComponent<Animator>();
+        attackCollider = GetComponent<PolygonCollider2D>();
+        fall.transform.position = startPoint.position;
+        StartCoroutine(FallMeteorCoroutine());
+    }
+
+    IEnumerator FallMeteorCoroutine()
+    {
+        fall.transform.position = startPoint.position;
+        fall.gameObject.SetActive(true);
+        explosion.gameObject.SetActive(false);
+
+        var tween = fall.transform.DOMove(endPoint.position, fallDuration)
+            .SetEase(Ease.InQuad).OnStepComplete(
+                () =>
+                {
+                    fall.gameObject.SetActive(false);
+                    explosion.gameObject.SetActive(true);
+                }
+            );
+        yield return tween.WaitForCompletion();
+        
+        // 폭발하는 동안 공격 콜라이더 활성화
+        attackCollider.enabled = true;
+        yield return new WaitUntil(() =>
+        {
+            return explosionAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1f;
+        });
+        attackCollider.enabled = false;
+        
+        Debug.Log($"Meteor Attack Finished {endPoint.position}");
+        Destroy(gameObject);
+        
+    }
+    
+    public void TakeDamage(TakeDamageEventArgs eventArgs)
+    {
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (isHitted) return;
+        
+        if(other.TryGetComponent(out Squad squad))
+        {
+            isHitted = true;
+            Debug.Log($"Meteor Hit {squad.name}");
+            squad.TakeDamage(new TakeDamageEventArgs(this, squad, damage));
+        }
+    }
+}
