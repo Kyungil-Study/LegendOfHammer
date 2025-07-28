188aaa3f05edf854ac997295b9c72f2e636dd3ed
gyusummer <gyusummer@gmail.com>
2025-07-28 15:26:15 +0900

    [용사단 관련 피드백]
    용사단 충돌 범위 조정
    캐릭터들 데미지 반올림 처리
    궁수 최종 데미지 증가 미적용 버그 수정
    궁수 관통 증강 표적 추가 데미지 버그 수정
    컨트롤 레버 프리팹화

--- Assets/0.Main/InGame/Scripts/Player/Archer/Archer.cs
+++ Assets/0.Main/InGame/Scripts/Player/Archer/Archer.cs
@@ -158,24 +158,17 @@ public class Archer : Hero
 
     // 궁수 기본 화살 피해량
     // [{(궁수 기본 공격 피해량 x 치명타 피해량) + 타격 당 데미지 + (궁수 기본 공격력 x 표적 대상 추가 피해 계수)} x 받는 피해량 증가] x 최종 데미지 증가
-    // TODO: 증강이 있는 경우에 대해서 구현해야함
     public override int CalculateDamage(bool isCritical = false)
     {
         float critFactor = isCritical ? squadStats.CriticalDamage : 1f;
-        return (int)(((baseAttackDamage * critFactor) + squadStats.BonusDamagePerHit + (baseAttackDamage * GetArcheryBonusEffectFactor()) * squadStats.FinalDamageFactor));
+        return Mathf.RoundToInt(((baseAttackDamage * critFactor) + squadStats.BonusDamagePerHit) * squadStats.FinalDamageFactor);
     }
     
     public int CalculateSubProjectileDamage(bool isCritical = false)
     {
-        var finalDamage = (int)(CalculateDamage(isCritical) * subProjectileAttackFactor);
+        var finalDamage = Mathf.RoundToInt(CalculateDamage(isCritical) * subProjectileAttackFactor);
         Debug.Log($"[Archer] SubProjectile Damage: {finalDamage}");
         return finalDamage;
         
-    }
-
-    // TODO: 증강에 의한 추가 계수
-    private float GetArcheryBonusEffectFactor()
-    {
-        return 0;
     }
 }
