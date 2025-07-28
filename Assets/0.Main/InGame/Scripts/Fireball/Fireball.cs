bf94ab92ffdd86b85d96cb43dd4a330b712a7cef
박승식 <agagtmdtlr>
2025-07-28 14:16:36 +0900

    [파이어 볼 맵 이벤트 추가]
    플레이 위치를 향해 전조 증상 후 발사
    플레이어와 충돌시 사라지고 피해를 입

---
+++ Assets/0.Main/InGame/Scripts/Fireball/Fireball.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using DG.Tweening.Core;
+using DG.Tweening.Plugins.Options;
+using TreeEditor;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+
+
+public class Fireball : MonoBehaviour , IBattleCharacter
+{
+    private static readonly int EXPLODE = Animator.StringToHash("Explode");
+    [SerializeField] Collider2D attackCollider; // 공격 범위 콜라이더
+    [SerializeField] private GameObject attackArea; // 타겟 레이어 마스크
+    [SerializeField] private Transform endPoint; // 공격 종료 지점
+    [SerializeField] private float attackDelay = 0.5f; // 공격 딜레이
+    [SerializeField] Animator fireballAnimator; // 파이어볼 애니메이션
+    [SerializeField] int damage = 10; // 예시로 10 데미지
+    [SerializeField] private float moveDuration; // 타겟 레이어 마스크
+    [SerializeField] float destroyDelay = 2f; // 애니메이션 재생 후 파괴될 시간
+    TweenerCore<Vector3, Vector3, VectorOptions> moveTween;
+    Coroutine moveCoroutine;
+    private void Start()
+    {
+        moveCoroutine = StartCoroutine(MoveCoroutine());
+    }
+
+    IEnumerator MoveCoroutine()
+    {
+        var destination = endPoint.position;
+        yield return new WaitForSeconds(attackDelay);
+        attackArea.gameObject.SetActive(false);
+        attackCollider.enabled = true; // 공격 범위 콜라이더 활성화
+        moveTween = transform.DOMove(destination, moveDuration);
+        yield return moveTween.WaitForCompletion();
+        Explode();
+    }
+
+    public void TakeDamage(TakeDamageEventArgs eventArgs)
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        attackCollider.enabled = false; // 공격 범위 콜라이더 비활성화 중복 피격 방지용
+        if (other.TryGetComponent<Squad>(out var target))
+        {
+            target.TakeDamage(new TakeDamageEventArgs(this, target, damage));
+        }
+        StopCoroutine(moveCoroutine);
+        Explode();
+    }
+
+    private void Explode()
+    {
+        if (moveTween != null)
+        {
+            moveTween.Kill();
+        }
+        fireballAnimator.SetTrigger(EXPLODE);
+        Destroy(gameObject, destroyDelay);
+    }
+
+}
