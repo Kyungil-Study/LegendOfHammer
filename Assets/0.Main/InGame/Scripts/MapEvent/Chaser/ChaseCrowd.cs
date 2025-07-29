790f7ca6019a930a800671d29888d77a812a6fe4
박승식 <agagtmdtlr>
2025-07-29 11:52:38 +0900

    [초기 추격대 나오는거 다시 원복]

--- Assets/0.Main/InGame/Scripts/MapEvent/Chaser/ChaseCrowd.cs
+++ Assets/0.Main/InGame/Scripts/MapEvent/Chaser/ChaseCrowd.cs
@@ -32,7 +32,8 @@ public class ChaseCrowd : MonoSingleton<ChaseCrowd>, IBattleCharacter
 
     private void Start()
     {
-        //StartCoroutine(ChaseCoroutine());
+        attackPower = 0; // 연출을 위한 초기 공격력 설정
+        StartCoroutine(ChaseCoroutine());
     }
 
 
