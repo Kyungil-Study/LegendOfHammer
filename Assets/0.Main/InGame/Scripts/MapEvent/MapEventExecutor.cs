18d03e596201fe2f57d5ce0839a608b057bdc320
박승식 <briankunkel9@gmail.com>
2025-07-31 10:08:01 +0900

    [사용안하는 맵이벤트 스크립트 정리]

--- Assets/0.Main/InGame/Scripts/MapEvent/MapEventExecutor.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class MapEventExecutor<T> : MonoSingleton<MapEventExecutor<T>>
-{
-    public abstract void ExecuteMapEvent();
-}
