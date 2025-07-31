6d8427c60ddf53bbb557e9c476f181c4d82debea
박승식 <briankunkel9@gmail.com>
2025-07-31 20:56:45 +0900

    [맵스크롤 수정]

--- Assets/0.Main/InGame/Scripts/Stage/PageScroller.cs
+++ Assets/0.Main/InGame/Scripts/Stage/PageScroller.cs
@@ -6,16 +6,44 @@ using UnityEngine;
 
 public class PageScroller : MonoBehaviour
 {
-    [SerializeField] private float scrollSpeed = 1.0f;
-    [SerializeField] private float scrollDistance = 10.0f;
-    [SerializeField] private PageSlot[] pages;
+    [SerializeField] private float nextPageInterval = 60f; // Time in seconds to show next page
+    [Space(10), SerializeField] private PageSlot[] pages;
+    [Space(10),SerializeField] StagePage[] stagePagePrefabs;
+
+    [SerializeField] MapSetting mapSetting;
     public PageSlot[] Pages => pages;
     bool[] reserveNextPage;
+    
+    bool isStarted = false;
 
     private void Awake()
     {
         reserveNextPage = Enumerable.Repeat(false, pages.Length).ToArray();
+        BattleEventManager.RegistEvent<ReadyBattleEventArgs>(OnReadyBattle);
+        BattleEventManager.RegistEvent<StartBattleEventArgs>(OnStartBattle);
         BattleEventManager.RegistEvent<NextPageEventArgs>(OnNextPage);
+        
+        viewHeight = Camera.main.orthographicSize * 2f;
+    }
+
+    private void OnReadyBattle(ReadyBattleEventArgs args)
+    {
+        int pageIndex = (args.StageIndex % stagePagePrefabs.Length);
+        int NextPageIndex = (pageIndex + 1) % stagePagePrefabs.Length;
+
+        foreach (var slot in Pages)
+        {
+            var page = Instantiate(stagePagePrefabs[pageIndex], slot.transform);
+            slot.AddPage(page);
+            var nextPage = Instantiate(stagePagePrefabs[NextPageIndex], slot.transform);
+            slot.AddPage(nextPage);
+            slot.NextPage();
+        }
+    }
+
+    private void OnStartBattle(StartBattleEventArgs args)
+    {
+        isStarted = true;
     }
 
     private void OnNextPage(NextPageEventArgs obj)
@@ -26,31 +54,39 @@ public class PageScroller : MonoBehaviour
         }
     }
 
+    public int startIndex;
+    public int endIndex;
+    
+    float viewHeight;
+
     // Update is called once per frame
     void Update()
     {
-        for (int i = 0; i < pages.Length; i++)
-        {
-            var page = pages[i];
-            
-            var topPosY = page.transform.position.y + scrollDistance;
-            if (topPosY <= -scrollDistance)
-            {
-                page.transform.position = new Vector3(
-                    page.transform.position.x,
-                    page.transform.position.y + scrollDistance*2*2,
-                    0);
-                
-                if(reserveNextPage[i])
-                {
-                    page.NextPage();
-                    reserveNextPage[i] = false;
-                }
-            }
-                
-            page.transform.position += Vector3.down * scrollSpeed * Time.deltaTime;
-        }
-     
+        if (!isStarted)
+            return;
         
+        Vector3 curPos = transform.position;
+        Vector3 nextPos = Vector3.down * mapSetting.ScrollSpeed * Time.unscaledDeltaTime;
+        transform.position = curPos + nextPos;
+
+        var endPage = pages[endIndex];
+        var endPageTransform = endPage.transform;
+        if (endPageTransform.position.y < -viewHeight)
+        {
+            Vector3 backSpritePos = endPageTransform.localPosition;
+            Vector3 frontSpritePos = endPageTransform.localPosition;
+            endPageTransform.localPosition
+                = backSpritePos + Vector3.up * viewHeight;
+            
+            if(reserveNextPage[endIndex])
+            {
+                endPage.NextPage();
+                reserveNextPage[endIndex] = false;
+            }
+            
+            int startIndexSave = startIndex;
+            startIndex = endIndex;
+            endIndex = (endIndex + 1) % pages.Length;
+        }
     }
 }
--- Assets/0.Main/InGame/Scripts/Stage/StageManager.cs
+++ Assets/0.Main/InGame/Scripts/Stage/StageManager.cs
@@ -10,9 +10,6 @@ public class StageManager : MonoBehaviour
     [Header("Stage 시간 세팅")]
     [SerializeField] private float nextPageInterval = 60f; // Time in seconds to show next page
     
-    [SerializeField] PageScroller pageScroller;
-    [SerializeField] StagePage[] stagePagePrefabs;
-    
     private float stageStartTime = 0f;
     Queue<StageWave> stageWavesQueue = new Queue<StageWave>();
 
@@ -24,7 +21,6 @@ public class StageManager : MonoBehaviour
 
     private void EndGame(EndBattleEventArgs args)
     {
-        pageScroller.enabled = false;
     }
 
     
@@ -35,33 +31,8 @@ public class StageManager : MonoBehaviour
         {
             stageWavesQueue.Enqueue(wave);
         }
-        
-        pageScroller.enabled = true;
-        int pageIndex = (startEventArgs.StageIndex % stagePagePrefabs.Length);
-        int NextPageIndex = (pageIndex + 1) % stagePagePrefabs.Length;
-
-        var pageSlots = pageScroller.Pages;
-        foreach (var slot in pageSlots)
-        {
-            var page = Instantiate(stagePagePrefabs[pageIndex], slot.transform);
-            slot.AddPage(page);
-            var nextPage = Instantiate(stagePagePrefabs[NextPageIndex], slot.transform);
-            slot.AddPage(nextPage);
-            
-            slot.NextPage();
-        }
-        
         stageStartTime = Time.time;
-        StartCoroutine(NextPageTimer(nextPageInterval));
         StartCoroutine(UpdateWave_Coroutine());
-    }
-
-    private IEnumerator NextPageTimer(float time)
-    {
-        yield return new WaitForSeconds(time);
-        Debug.Log("Next page triggered.");
-        NextPageEventArgs args = new NextPageEventArgs();
-        BattleEventManager.CallEvent(args);
     }
 
     private IEnumerator UpdateWave_Coroutine()
