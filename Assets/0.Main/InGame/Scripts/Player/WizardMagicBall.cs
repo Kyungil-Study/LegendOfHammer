5b997dab189a29713d572af8776d1f1d61290d8b
DESKTOP-ICE249B\mhs99 <https://github.com/mhs0209>
2025-08-05 21:05:55 +0900

    [도트딜 디버프 + 최종 데미지 증가 영향 받게 수정]

--- Assets/0.Main/InGame/Scripts/Player/WizardMagicBall.cs
+++ Assets/0.Main/InGame/Scripts/Player/WizardMagicBall.cs
@@ -28,7 +28,7 @@ public class WizardMagicBall : HeroProjectile<Wizard>
             );
             BattleEventManager.CallEvent(eventArgs);
             enemy.Stat.AddModifier(new DamageAmpModifier(Owner.DebuffRate, Owner.DebuffDuration));
-            float percent = Owner.Dot_HP_Ratio;              
+            float percent = Owner.Dot_HP_Ratio*Owner.SquadStats.FinalDamageFactor;              
             float dps     = enemy.Stat.MaxHP * percent;      
             enemy.Stat.AddModifier(new DamageOverTimeModifier(dps, Owner.Dot_HP_Ratio_Duration));
             Debug.Log($"폭발 도트 {Owner.Dot_HP_Ratio}, {Owner.Dot_HP_Ratio_Duration}");
